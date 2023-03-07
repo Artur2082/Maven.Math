a7d21b8521e5117b3e9e50aa460d8e0eed552e53
Artur Minasian <Arthrmin@gmail.com>
2023-03-07 22:06:02 +0200

    Initial commit

---
+++ src/main/java/org/example/Maven/Math/Math_Methods.java
@@ -0,0 +1,35 @@
+package org.example.Maven.Math;
+
+public class Math_Methods {
+   public int sum(int a, int b){
+       return a + b ;
+   }
+
+    public int difference(int a, int b){
+        return a - b ;
+    }
+
+    public int multiplication(int a, int b){
+        return a * b ;
+    }
+
+    public double division(double a, double b){
+        return a / b ;
+    }
+
+    public double squareRoot(double a){
+        return Math.sqrt(a);
+    }
+
+    public double degree(double a, double b){
+        return Math.pow(a, b);
+    }
+
+    public double sin(double a){
+        return Math.sin(a);
+    }
+
+    public double cos(double a){
+        return Math.cos(a);
+    }
+}
